a3cc36ae4bc8ff66aa40794ffb2157b13801a94e
matt765 <mateusz.wyrebek@gmail.com>
2025-10-09 23:58:34 +0200

    Improve small cards design

--- src/components/views/homepage/HomeSmallCards.tsx
+++ src/components/views/homepage/HomeSmallCards.tsx
@@ -35,30 +35,32 @@ export const HomeSmallCards = ({ homeSmallCardsData }: HomeSmallCardsProps) => {
             <div className="flex small-box max-[420px]:-ml-3 flex-col sm:flex-row justify-between md:justify-normal">
               <div className="flex flex-col w-full sm:w-1/2 mt-[0.35rem] lg:mt-0 lg:gap-1 pl-1 sm:pl-0">
                 <div className="flex flex-row lg:flex-col items-center lg:items-start">
-                  <div className="text-primaryText font-medium text-md sm:text-md lg:text-xs 1xl:text-sm tracking-tight flex sm:block items-center lg:mr-0 mr-2 mb-1">
+                  <div className="text-primaryText font-medium text-md sm:text-md lg:text-xs 1xl:text-sm tracking-tight flex sm:block items-center lg:mr-0 mr-2 mb-0 mt-1">
                     {item.title}
                   </div>
-                  <div className="pb-1 lg:pb-0 text-[1.1rem] lg:text-[1.2rem] lg:text-md 1xl:text-[1.15rem] 3xl:text-xl font-semibold text-primaryText flex">
+                </div>
+                <div className="flex items-center gap-4">
+                  <div className="pb-1 lg:pb-0 text-[1.1rem] lg:text-[1.2rem] lg:text-md 1xl:text-[1.15rem] 3xl:text-[2rem] font-semibold text-primaryText flex">
                     {item.metric}
                   </div>
-                </div>
-                <div className="flex">
-                  {item.increased ? (
-                    <div className="text-xs text-green-600">
-                      +{item.changeValue}%
+                  <div className="flex">
+                    {item.increased ? (
+                      <div className="text-xs text-green-600">
+                        +{item.changeValue}%
+                      </div>
+                    ) : (
+                      <div className="text-xs text-red-500">
+                        -{item.changeValue}%
+                      </div>
+                    )}
+                    <div className="flex lg:hidden xl:flex text-xs text-gray-400 ml-1 whitespace-nowrap">
+                      {item.changeText}
                     </div>
-                  ) : (
-                    <div className="text-xs text-red-500">
-                      -{item.changeValue}%
-                    </div>
-                  )}
-                  <div className="flex lg:hidden xl:flex text-xs text-gray-400 ml-1 whitespace-nowrap">
-                    {item.changeText}
                   </div>
                 </div>
               </div>
               <div className="w-full sm:w-[40%] md:w-[45%] lg:w-[50%] xl:w-[40%] 2xl:w-1/2 md:pl-3 md:ml-2 2xl:ml-0 sm:flex mt-4 sm:mt-0 pr-0 2xl:pr-2 sm:pr-0">
-                <Chart chartData={chartArray} color={item.color} />
+                {/* <Chart chartData={chartArray} color={item.color} /> */}
               </div>
             </div>
           </Card>
