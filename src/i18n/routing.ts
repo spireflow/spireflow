f63b708ab2f84856a9480ca3833eee3d03509478
Benjamin Thiele <benjamin@otherwise.gg>
2026-05-23 22:48:45 +0200

    Add German locale to routing configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "pl"],
+  locales: ["en", "pl", "de"],
   defaultLocale: "en",
   localePrefix: "as-needed",
   localeDetection: false,
